65cb664bdce5e179ccca9d4feb77eda56fc12cf5
Anton <antons0vv@gmail.com>
2025-02-10 23:31:34 +0300

    update

--- javascript/scandata.js
+++ javascript/scandata.js
@@ -1,13 +1,22 @@
 // Scans data
 let scanData = {
 
-    AgACAgIAAxkBAAMVZ6k2IsdI_p9AJ1nDcumOcObt8_QAAkvxMRsulElJqk42xzpuzpYBAAMCAAN4AAM2BA_12________2020_______: {
-        name: "test1",
-        date: "12 апреля 2020/ Город",
-        description: "Test1",
-        src: "img/AgACAgIAAxkBAAMVZ6k2IsdI_p9AJ1nDcumOcObt8_QAAkvxMRsulElJqk42xzpuzpYBAAMCAAN4AAM2BA.jpg",
+    unknown_001_2020: {
+        name: "hey",
+        date: "12 декабрь 2020/ Город",
+        description: "dad",
+        src: "img/AgACAgIAAxkBAAPKZ6phe_q_ZCeJqeqiPaV7P48GVSgAAk3vMRus21BJkilAp5KY8D4BAAMCAAN5AAM2BA.jpg",
     },
-        post_easter: {
+    
+    spring_001_2020: {
+        name: "mew",
+        date: "12 апреля 2020/ Город",
+        description: "hew",
+        src: "img/AgACAgIAAxkBAANpZ6ohpFO_S-dO4EZ2U_PVUiuQNzEAApPuMRsulFFJXyB6oD1tN3cBAAMCAAN5AAM2BA.jpg",
+    },
+    
+
+    post_easter: {
         name: "Пасхальная открытка",
         date: "12 апреля 2020/ Санкт-Петербург",
         description: "Получил в Анненкирхе на пасху",
